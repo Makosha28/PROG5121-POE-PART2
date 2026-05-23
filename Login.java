30e29a120d0d85ceefad21a49c74f3d648b1e22d
Makosha28 <st10534919@rcconnect.edu.za>
2026-05-23 17:43:05 +0200

    Remove commented code and clean up Login class

--- Login.java
+++ Login.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.quickchat;
 
-/**
- *
- * @author mamphekgonthabiseng
- */
+package com.mycompany.quickchat;
 import java.util.Scanner;
 
 public class Login {
