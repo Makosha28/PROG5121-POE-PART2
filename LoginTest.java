3bc641dfe01152b5924c750161948ddafda6ce58
Makosha28 <st10534919@rcconnect.edu.za>
2026-05-23 17:45:45 +0200

    Remove unnecessary whitespace in LoginTest.java

--- LoginTest.java
+++ LoginTest.java
@@ -9,10 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author mamphekgonthabiseng
- */
+
 public class LoginTest {
     
     public LoginTest() {
